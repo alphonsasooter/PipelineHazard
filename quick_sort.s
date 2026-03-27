71f0f7222e3e39b9c4ca9161f28ff4131d689302
Alphonsa <alphonsasooter@gmail.com>
2026-03-27 13:02:58 +0530

    Enhance quick_sort with pivot and swap functionality
    Refactor quick_sort implementation to include pivot selection and swapping logic.

--- quick_sort.s
+++ quick_sort.s
@@ -1,16 +1,48 @@
+.data
+array: .word 9, 3, 7, 1, 5
+n:     .word 5
+
 .text
 .globl main
 main:
-    li x10, 5
-    li x11, 3
+    la x10, array
+    lw x11, n
 
-    blt x10, x11, less   # ?? CONTROL HAZARD
+    li x12, 0              # low
+    addi x13, x11, -1      # high
 
-    addi x12, x0, 1
-    j end
+    # pivot = array[high]
+    slli x14, x13, 2
+    add x15, x10, x14
+    lw x16, 0(x15)
 
-less:
-    addi x12, x0, 2
-next
-end:
+    addi x17, x12, -1      # i = low -1
+    mv x18, x12            # j = low
+
+loop:
+    bge x18, x13, done
+
+    slli x19, x18, 2
+    add x20, x10, x19
+    lw x21, 0(x20)
+
+    blt x21, x16, swap     # control hazard
+
+    addi x18, x18, 1
+    j loop
+
+swap:
+    addi x17, x17, 1
+
+    slli x22, x17, 2
+    add x23, x10, x22
+    lw x24, 0(x23)
+
+    sw x21, 0(x23)
+    sw x24, 0(x20)
+
+    addi x18, x18, 1
+    j loop
+
+done:
     nop
